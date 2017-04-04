1e2951f4b460cf756ff53ce82af271883f533a29
freebreaker <592378845@qq.com>
2017-04-04 19:14:24 +0800

    使用webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 
 
 var path = require('path');
-
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
   entry: './app/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins:[
+      new UglifyJSPlugin({
+          comments: false
+      })
+  ]
 };
